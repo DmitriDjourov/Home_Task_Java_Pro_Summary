43fd97cbf7948b9e1cc23381797fef94a41230d0
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2023-09-21 18:48:32 +0300

    Delete added code . All bugs fixed.

--- HWS_08092023/src/de/telran/lesson20230908/Pet.java
+++ HWS_08092023/src/de/telran/lesson20230908/Pet.java
@@ -2,7 +2,6 @@ package de.telran.lesson20230908;
 
 public abstract class Pet extends Animal {
 
-  // static boolean isHungry;
   public Pet(String name, int age) {
     super(name, age);
     setHungry(false);
