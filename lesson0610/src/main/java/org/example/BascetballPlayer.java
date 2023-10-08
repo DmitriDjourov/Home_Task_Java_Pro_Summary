97e52ef38781ba21f0bf1adeaddd93c3078fdcfd
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2023-10-08 23:09:42 +0300

    control commit

--- lesson0610/src/main/java/org/example/BascetballPlayer.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class BascetballPlayer implements Playable {
-		@Override
-		public void play(){
-				System.out.println("Playing bascetboll");
-		}
-}
--- lesson0610/src/main/java/org/example/Footballplayer.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Footballplayer implements Playable{
-		@Override
-		public void play(){
-				System.out.println("Playing Football ");
-		}
-}
